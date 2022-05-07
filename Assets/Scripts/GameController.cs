5f11625186c012b2885eca20a5ae32bac9dc0ca8
Wannacr7 <jefersonalvarezmesa@gmail.com>
2022-05-07 17:56:42 -0500

    Staging  vfx 1

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private Animator modelAnim;
+    [SerializeField] private ParticleSystem parSys;
+    [SerializeField] private ParticleSystem parSys2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("Space works");
+            modelAnim.SetTrigger("Spell");
+           // parSys2.Play();
+        }
+    }
+    public void startVFX()
+    {
+        Debug.Log("Start VFX");
+        parSys.Play();
+    }
+}
